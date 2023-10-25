2b4dde2d62afec11d59bbcc82fbc409694c51054
poornapragnyah <nightfury20203@gmail.com>
2023-10-25 20:50:16 +0530

    navlink not wrapping

--- src/components/ShopCard.jsx
+++ src/components/ShopCard.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const ShopCard = (props) => {
   return (
-    <a href={props.href} target='_blank'>
-    <div className='bg-white inline-block p-4 m-4 rounded-md bounce cursor-pointer shadow-md hover:scale-105 ease-in-out w-72 relative'>
+    <NavLink to={props.href}>
+    <div className='bg-white inline-block p-4 m-4 rounded-md bounce cursor-pointer shadow-md hover:scale-105 ease-in-out w-72 relative -z-10'>
       <img src={props.src} alt={props.caption} className='rounded-md mb-1 shadow-sm h-44'/>
       <div className='p-2 '>
         <p className='text-2xl bg rounded-md font-semibold'>{props.caption}</p>
@@ -14,7 +15,7 @@ const ShopCard = (props) => {
             <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
         </svg>
     </div>
-    </a>
+    </NavLink>
   )
 }
 
